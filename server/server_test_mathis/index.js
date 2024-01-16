02d78eacc44e147130b252c3481c2bd701db01ca
MathisVermeren <vermerenus@gmail.com>
2024-01-16 12:13:27 +0100

    server fetch de github

---
+++ server/server_test_mathis/index.js
@@ -0,0 +1,23 @@
+const githubRepoUrl = 'https://raw.githubusercontent.com/MathisVermeren/MurImage/main/content.json';
+
+async function fetchJsonFile() {
+  try {
+    const response = await fetch(githubRepoUrl);
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP ${response.status} - ${response.statusText}`);
+    }
+
+    const jsonData = await response.json();
+
+    console.log('Contenu du fichier JSON:');
+    console.log(jsonData);
+
+    // Faites ce que vous voulez avec les données JSON ici
+
+  } catch (error) {
+    console.error('Une erreur s\'est produite lors de la récupération du fichier JSON:', error.message);
+  }
+}
+
+fetchJsonFile();
