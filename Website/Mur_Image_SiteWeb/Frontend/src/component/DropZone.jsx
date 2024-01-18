097c1d4a928c78bd65e97929206bd0fdf624ed19
MathisVermeren <vermerenus@gmail.com>
2024-01-18 17:19:18 +0100

    DropZone.jsx

--- Website/Mur_Image_SiteWeb/Frontend/src/component/DropZone.jsx
+++ Website/Mur_Image_SiteWeb/Frontend/src/component/DropZone.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import './DropZone.css';
 
-const DropZone = () => {
-  const [file, setFile] = useState(null);
+const DropZone = (file, setFile) => {
 
   const handleDrop = (event) => {
     event.preventDefault();
@@ -16,31 +15,11 @@ const DropZone = () => {
     setFile(selectedFile);
   };
 
-  const handleUpload = () => {
-    if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
-
-      fetch('http://localhost:4800/uploads', {
-        method: 'POST',
-        body: formData,
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Image envoyée avec succès:', data);
-        })
-        .catch(error => {
-          console.error('Erreur lors de l\'envoi de l\'image:', error);
-        });
-    }
-  };
-
   return (
     <div className="drop-zone" onDrop={handleDrop} onDragOver={(e) => e.preventDefault()}>
       {file ? (
         <div className="preview">
           <img src={URL.createObjectURL(file)} alt="Preview" />
-          <button onClick={handleUpload}>Envoyer</button>
         </div>
       ) : (
         <div className="instructions">
